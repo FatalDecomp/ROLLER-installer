fb839d2cdd50fa9dceb2d022e2177db4e556cb8b
Simon Hartcher <simon@simonhartcher.com>
2025-09-06 21:55:58 +1000

    build: fix windows build arguments

--- scripts/build.py
+++ scripts/build.py
@@ -44,16 +44,13 @@ def main():
     # Use different flags based on platform
     if sys.platform == "win32":
         # Simplified build for Windows - avoid problematic optimizations
-        pyinstaller_cmd = """
-        poetry run pyinstaller \
-          --onefile \
-          --name roller-installer \
-          --clean \
-          --hidden-import roller_installer.core.handlers.zip_handler \
-          --hidden-import roller_installer.core.handlers.iso_handler \
-          --hidden-import roller_installer.core.handlers.cue_bin_handler \
-          main.py
-        """
+        pyinstaller_cmd = (
+            "poetry run pyinstaller --onefile --name roller-installer --clean "
+            "--hidden-import roller_installer.core.handlers.zip_handler "
+            "--hidden-import roller_installer.core.handlers.iso_handler "
+            "--hidden-import roller_installer.core.handlers.cue_bin_handler "
+            "main.py"
+        )
     else:
         # Full optimizations for Unix systems
         pyinstaller_cmd = """
